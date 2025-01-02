28d870450d3289827840c945927dd4125b6e81a9
skj092 <sonujha092@gmail.com>
2025-01-02 22:43:53 +0530

    minor bug fix

--- src/engine.py
+++ src/engine.py
@@ -5,7 +5,6 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def train_one_epoch(train_dl, model, loss_fn, optimizer):
-    accumulation_steps = 32
     model.train()
     running_loss = 0
     for itr, batch in enumerate(tqdm(train_dl)):
@@ -15,19 +14,17 @@ def train_one_epoch(train_dl, model, loss_fn, optimizer):
         outputs = model(images)
         loss = loss_fn(outputs, targets)
         loss.backward()
-        if (itr + 1) % accumulation_steps == 0:
-            optimizer.step()
-            optimizer.zero_grad()
+        optimizer.step()
+        optimizer.zero_grad()
 
         running_loss += loss.item()
         outputs = outputs.detach().cpu()
-    epoch_loss = (running_loss * accumulation_steps) / len(train_dl)
+    epoch_loss = (running_loss) / len(train_dl)
     return epoch_loss
 
 
 def validate_one_epoch(valid_dl, model, loss_fn, optimizer):
     with torch.no_grad():
-        accumulation_steps = 32
         model.eval()
         running_loss = 0
         for itr, batch in enumerate(tqdm(valid_dl)):
@@ -38,5 +35,5 @@ def validate_one_epoch(valid_dl, model, loss_fn, optimizer):
             loss = loss_fn(outputs, targets)
             running_loss += loss.item()
             outputs = outputs.detach().cpu()
-        epoch_loss = (running_loss * accumulation_steps) / len(valid_dl)
+        epoch_loss = (running_loss) / len(valid_dl)
     return epoch_loss
