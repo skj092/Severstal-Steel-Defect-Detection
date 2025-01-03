f818e6a99e66b4e96508f4bd0fc5002c772f1665
skj092 <sonujha092@gmail.com>
2025-01-03 07:58:09 +0530

    adding metric to log

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import torch
+from utils import compute_iou_batch, Meter, epoch_log
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -7,6 +8,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def train_one_epoch(train_dl, model, loss_fn, optimizer):
     model.train()
     running_loss = 0
+    iou = 0
     for itr, batch in enumerate(tqdm(train_dl)):
         images, targets = batch
         images, targets = images.to(device), targets.to(device)
@@ -19,13 +21,18 @@ def train_one_epoch(train_dl, model, loss_fn, optimizer):
 
         running_loss += loss.item()
         outputs = outputs.detach().cpu()
+        iou += compute_iou_batch(outputs, targets)
     epoch_loss = (running_loss) / len(train_dl)
-    return epoch_loss
+    epoch_iou = iou / len(train_dl)
+
+    return epoch_loss, epoch_iou
 
 
 def validate_one_epoch(valid_dl, model, loss_fn, optimizer):
     model.eval()
+    meter = Meter()
     running_loss = 0
+    iou = 0
     for itr, batch in enumerate(tqdm(valid_dl)):
         images, targets = batch
         images, targets = images.to(device), targets.to(device)
@@ -35,5 +42,9 @@ def validate_one_epoch(valid_dl, model, loss_fn, optimizer):
             loss = loss_fn(outputs, targets)
         running_loss += loss.item()
         outputs = outputs.detach().cpu()
+        iou += compute_iou_batch(outputs, targets)
+        meter.update(targets, outputs)
     epoch_loss = (running_loss) / len(valid_dl)
-    return epoch_loss
+    epoch_iou = iou / len(valid_dl)
+    epoch_log(epoch_loss, meter)
+    return epoch_loss, epoch_iou
--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,105 @@ def mask2rle(mask):
     return " ".join(str(x) for x in runs)
 
 
+def metric(probability, truth, threshold=0.5, reduction='none'):
+    '''Calculate dice of positive and negative images seperately'''
+    '''probability and truth must be torch tensor'''
+    batch_size = len(truth)
+    with torch.no_grad():
+        probability = probability.view(batch_size, -1)
+        truth = truth.view(batch_size, -1)
+        assert (probability.shape == truth.shape)
+
+        p = (probability > threshold).float()
+        t = (truth > 0.5).float()
+
+        t_sum = t.sum(-1)
+        p_sum = p.sum(-1)
+
+        neg_index = torch.nonzero(t_sum == 0)
+        pos_index = torch.nonzero(t_sum >= 1)
+
+        dice_neg = (p_sum == 0).float()
+        dice_pos = 2 * (p*t).sum(-1)/((p+t).sum(-1))
+
+        dice_neg = dice_neg[neg_index]
+        dice_pos = dice_neg[pos_index]
+        dice = torch.cat([dice_pos, dice_neg])
+
+        num_neg = len(neg_index)
+        num_pos = len(pos_index)
+
+    return dice, dice_neg, dice_pos, num_neg, num_pos
+
+
+def predict(X, threshold):
+    '''X is sigmoid outout of the model'''
+    X_p = np.copy(X)
+    preds = (X_p > threshold).astype('uint8')
+    return preds
+
+
+def compute_ious(pred, label, classes, ignore_index=255, only_present=True):
+    '''computes iou for one ground truth mask and predicted mask'''
+    pred[label == ignore_index] == 0
+    ious = []
+    for c in classes:
+        label_c = label == c
+        if only_present and np.sum(label_c) == 0:
+            ious.append(np.nan)
+            continue
+        pred_c = pred = c
+        intersection = np.logical_and(pred_c, label_c).sum()
+        union = np.logical_or(pred_c, label_c).sum()
+        if union != 0:
+            ious.append(intersection / union)
+    return ious if ious else [1]
+
+
+def compute_iou_batch(outputs, labels, classes=None):
+    '''computes mean iou for batch of ground truth masks and predicted masks'''
+    ious = []
+    preds = np.copy(outputs)
+    labels = np.array(labels)
+    for pred, label in zip(preds, labels):
+        ious.append(np.nanmean(compute_ious(pred, label, classes)))
+    iou = np.nanmean(ious)
+    return iou
+
+
+class Meter(object):
+    '''A meter to keep track of iou and dice scores through an epoch'''
+
+    def __init__(self, epoch):
+        self.base_threshold = 0.5
+        self.base_dice_scores = []
+        self.dice_neg_scores = []
+        self.dice_pos_scores = []
+        self.iou_scores = []
+
+    def update(self, targets, outputs):
+        probs = torch.sigmoid(outputs)
+        dice, dice_neg, dice_pos, _, _ = metric(
+            probs, targets, self.base_threshold)
+        self.base_dice_scores.extend(dice.tolist())
+        self.dice_pos_scores.extend(dice_pos.tolist())
+        self.dice_neg_scores.extend(dice_neg.tolist())
+        preds = predict(probs, self.base_threshold)
+        iou = compute_iou_batch(preds, targets, classes=[1])
+        self.iou_scores.append(iou)
+
+    def get_metric(self):
+        dice = np.nanmean(self.base_dice_scores)
+        dice_neg = np.nanmean(self.dice_neg_scores)
+        dice_pos = np.nanmean(self.dice_pos_scores)
+        dices = [dice, dice_neg, dice_pos]
+        iou = np.nanmean(self.iou_scores)
+        return dices, iou
+
+
+def epoch_log(epoch_loss, meter):
+    '''loggint the metrics at the end of an epoch'''
+    dices, iou = meter.get_metric()
+    dice, dice_neg, dice_pos = dices
+    print(f"Loss: {loss} | IoU {iou} | dice {dice} | dice_neg {dice_neg} | dice_pos {dice_pos}")
+    return dice, iou
